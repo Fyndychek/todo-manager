421be887dc5d441a44f91d32b7ccf4217344e4e9
Fyndychek <alex04.05.05.aa@gmail.com>
2026-08-11 10:17:13 +0300

    Simple server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Todo struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	Created   time.Time `json:"created_at"`
+}
+
+var (
+	todos   = []Todo{}
+	mu      sync.Mutex
+	counter = 0
+)
+
 func main() {
+	port := 8080
+	if p := os.Getenv("PORT"); p != "" {
+		if v, err := strconv.Atoi(p); err == nil {
+			port = v
+		}
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Todo Api 1.0.0")
+	})
+
+	log.Printf("Server on :%d", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 
 }
